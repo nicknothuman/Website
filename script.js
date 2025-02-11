d9b20f3a4841f5cd4146f4255030f5cf739512aa
nicknothuman <nicolasleroux2010off@gmail.com>
2025-02-11 18:24:44 -0500

    Add files via upload

--- script.js
+++
@@ -1,5 +0,0 @@
-<div>
-   <iframe src="https://scratch.mit.edu/projects/105500895/embed" frameborder="0" width="70%" height="700" ></iframe>
-  <iframe src="https://scratch.mit.edu/projects/1114821633/embed" frameborder="0"></iframe>
-   <iframe src="https://www.bing.com/search?adlt=strict&cvid=1D4369621DB04CA187F25447EE50357F&form=QBLH&ghacc=0&ghpl&ghsh=0&lq=0&pq&q=h&qs=n&sc=0-0&sk&sp=-1&safesearch=strict&qft=filterui:license-L2_L3/embed" frameborder="0" width="100%" height="400" class="bing"></iframe>
-  </div>
